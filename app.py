fd1e6924221049fe480d4755ea999e32ac58b094
OnionPowder01 <autonatione36@gmail.com>
2023-03-21 13:59:43 +0200

    backend

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 from bson import json_util
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 load_dotenv(find_dotenv())
 password = os.environ.get("MONGODB_PWD")
